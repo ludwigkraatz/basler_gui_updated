73ace746d003c3f242194b0d6aac6185004fcf37
Davor Virag <davor.virag@gmail.com>
2024-02-23 13:11:28 +0100

    each cam has its own writer thread, added "time.sleep(0)" for yielding to other threads

--- b_record_all_cams.py
+++ b_record_all_cams.py
@@ -24,6 +24,7 @@ import cv2
 import platform
 from bedsy.bedsy import Bedsy
 import queue
+from collections import deque
 
 if platform.system() == 'Windows':
     from reset_USB import reset_baslers_windows as reset_baslers
@@ -56,8 +57,10 @@ class BaslerMouseRecorder():
         self.ffmpeg_command = ffmpeg
         self.manager_running = False
         self.writers_running = False
-        self.writer_thread = None
+        #self.writer_thread = None
+        self.c_threads = dict()
         self.use_dummy_camera = False
+        self.writers_ready = {}
         reset_baslers()
 
     def set_logfile(self):
@@ -113,44 +116,88 @@ class BaslerMouseRecorder():
             result.append("->Resulting Frame Rate: {}\n".format(cam.ResultingFrameRate.Value))
         return "".join(result)
 
-    def start_writing_frames(self):
-        self.writer_ready = False
+    # def start_writing_frames(self):
+    #     self.writer_ready = False
+    #     if self.use_bedsy:
+    #         self.cameras.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+    #     else:
+    #         self.cameras.StartGrabbing()
+    #     #time.sleep(3)
+    #     first_frame = True
+    #     while self.writers_running:
+    #         if self.use_dummy_camera:
+    #             for c in self.cameras:
+    #                 if c.DeviceInfo.GetDeviceClass() == "BaslerCamEmu":
+    #                     c.ExecuteSoftwareTrigger()
+    #         if self.use_bedsy:
+    #             if first_frame:
+    #                 while not all([c.AcquisitionStatus.GetValue() for c in self.cameras]):
+    #                     time.sleep(0.1)
+    #                 first_frame = False
+    #                 self.writer_ready = True
+    #                 self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
+    #             grabResult = self.cameras.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+    #         else:
+    #             grabResult = self.cameras.RetrieveResult(500, pylon.TimeoutHandling_ThrowException)
+    #             self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
+    #         serial = self.cameras[grabResult.GetCameraContext()].DeviceInfo.GetSerialNumber()
+    #         # Write image to video
+    #         frame = grabResult.GetArray()
+    #         self.frames[serial] = frame
+    #         self.writers[serial].write_frame(frame)
+    #         grabResult.Release()
+    #         self.frame_counter_dict[serial] += 1
+    #     self.end_t = time.time()
+    #     self.cameras.StopGrabbing()
+
+    def cam_start_writing_frames(self, c, serial):
         if self.use_bedsy:
-            self.cameras.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+            c.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
         else:
-            self.cameras.StartGrabbing()
+            c.StartGrabbing()
         #time.sleep(3)
         first_frame = True
         while self.writers_running:
             if self.use_dummy_camera:
-                for c in self.cameras:
-                    if c.DeviceInfo.GetDeviceClass() == "BaslerCamEmu":
-                        c.ExecuteSoftwareTrigger()
+                if c.DeviceInfo.GetDeviceClass() == "BaslerCamEmu":
+                    c.ExecuteSoftwareTrigger()
             if self.use_bedsy:
                 if first_frame:
-                    while not all([c.AcquisitionStatus.GetValue() for c in self.cameras]):
-                        time.sleep(0.1)
+                    while not c.AcquisitionStatus.GetValue():
+                        time.sleep(0)
                     first_frame = False
-                    self.writer_ready = True
-                    self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
-                grabResult = self.cameras.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+                    self.writers_ready[serial] = True
+                    #self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
+                grabResult = c.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
             else:
-                grabResult = self.cameras.RetrieveResult(500, pylon.TimeoutHandling_ThrowException)
-                self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
-            serial = self.cameras[grabResult.GetCameraContext()].DeviceInfo.GetSerialNumber()
+                grabResult = c.RetrieveResult(500, pylon.TimeoutHandling_ThrowException)
+                #self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
+            #serial = self.cameras[grabResult.GetCameraContext()].DeviceInfo.GetSerialNumber()
             # Write image to video
             frame = grabResult.GetArray()
-            self.frames[serial] = frame
+            #self.frames[serial] = frame
+            self.frames[serial].append(frame)
             self.writers[serial].write_frame(frame)
             grabResult.Release()
             self.frame_counter_dict[serial] += 1
-        self.end_t = time.time()
-        self.cameras.StopGrabbing()
+            time.sleep(0)
+        #self.end_t = time.time()
+        c.StopGrabbing()
 
-    def start_writing_frames_in_thread(self):
-        self.writer_thread = threading.Thread(target=self.start_writing_frames, args=())
-        self.writer_thread.daemon = True
-        self.writer_thread.start()
+    # def start_writing_frames_in_thread(self):
+    #     self.writer_thread = threading.Thread(target=self.start_writing_frames, args=())
+    #     self.writer_thread.daemon = True
+    #     self.writer_thread.start()
+        
+    def cam_start_writing_frames_in_thread(self):
+        for c in self.cameras:
+            serial = c.GetDeviceInfo().GetSerialNumber()
+            self.c_threads[serial] = threading.Thread(target=self.cam_start_writing_frames, args=(c, serial))
+            self.c_threads[serial].daemon = True
+            self.writers_ready[serial] = False
+            self.c_threads[serial].start()
+            self.frames[serial] = deque(maxlen=1)
+        self.start_t = self.logger.logWithTime("Started recording with {} Basler camera{}.".format(self.num_cams, 's' if self.num_cams>1 else ''), stdout=True)
 
     def start_recording(self):
         self.set_logfile()
@@ -222,11 +269,11 @@ class BaslerMouseRecorder():
                         self.writers[serial] = r2v.FFMPEG_VideoWriter(vid_fname, self.size, fps=cam.ResultingFrameRate.Value, pixfmt=pixfmt, ffmpeg_command=self.ffmpeg_command)
                     self.frame_counter_dict[serial] = 0
                 # Start grabbing and writing to video file
-                self.start_writing_frames_in_thread()
+                self.cam_start_writing_frames_in_thread()
                 self.logger.logWithTime("Started recording.", stdout=True)
                 if self.use_bedsy and not bedsy_initialised:
                     #print("DEBUG","Hello")
-                    while not self.writer_ready:
+                    while not all(self.writers_ready):
                         time.sleep(0)
                     #print("DEBUG", "Starting BeDSy...")
                     bedsy.start_bedsy()
@@ -255,22 +302,28 @@ class BaslerMouseRecorder():
                 while getattr(recmanager_thread, "thread_running") and not do_rollover:
                     for serial in self.serials:
                         if serial in self.frames:
-                            cv2.imshow(f'Basler {serial}', self.frames[serial])
+                            try:
+                                cv2.imshow(f'Basler {serial}', self.frames[serial].pop())
+                            except IndexError:
+                                pass
                     if self.use_bedsy:
                         try:
                             # try to get a message from the queue
                             # they will be tuples:
                             # first item: isoformat timestamp, second item: message
-                            msg = q.get(timeout=0.1)
+                            msg = q.get(block=False)
                         except queue.Empty:
                             cv2.waitKey(1)
+                            time.sleep(0)
                         else:
                             if "[STOP_ROLLOVER]" in msg[1]:
                                 self.writers_running = False
                                 self.end_t = time.time()
                                 self.logger.logWithTime("Recording rollover...", stdout=True)
                                 cv2.destroyAllWindows()
-                                self.writer_thread.join()
+                                #self.writer_thread.join()
+                                for t in self.c_threads:
+                                    t.join()
                                 for writer in self.writers.values(): writer.close()
                                 self.frames = dict()
                                 frame_avg = sum([f for f in self.frame_counter_dict.values()]) / len(self.frame_counter_dict)
@@ -282,6 +335,7 @@ class BaslerMouseRecorder():
                                 do_rollover = True
                     else:
                         cv2.waitKey(750) # ms
+                        time.sleep(0)
 
         finally: # Clean up and log the fps
             self.writers_running = False
@@ -301,8 +355,13 @@ class BaslerMouseRecorder():
             time.sleep(1)
             cv2.destroyAllWindows()
             #self.cameras.StopGrabbing()
-            if self.writer_thread is not None and self.writer_thread.is_alive(): self.writer_thread.join()
-            self.writer_thread = None
+            #if self.writer_thread is not None and self.writer_thread.is_alive(): self.writer_thread.join()
+            #self.writer_thread = None
+            for t in self.c_threads:
+                if t.is_alive():
+                    t.join()
+            self.end_t = time.time()
+            self.c_threads = dict()
             for writer in self.writers.values(): writer.close()
             frame_avg = sum([f for f in self.frame_counter_dict.values()]) / len(self.frame_counter_dict)
             #if not self.use_bedsy:
